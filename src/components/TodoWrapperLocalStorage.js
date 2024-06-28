e19989cbedde2846e7215faec3ffa9b07eddb9a7
roganth-IT21 <roganth.it21@bitsathy.ac.in>
2024-06-28 21:20:31 +0530

    first commit

---
+++ src/components/TodoWrapperLocalStorage.js
@@ -0,0 +1,25 @@
+import React, {useState, useEffect} from 'react'
+import { TodoForm } from './TodoForm'
+import { v4 as uuidv4 } from 'uuid';
+import { Todo } from './Todo';
+import { EditTodoForm } from './EditTodoForm';
+uuidv4();
+
+export const TodoWrapperLocalStorage = () => {
+    
+  return (
+    <div className='TodoWrapper'>
+        <h1>Get Things Done!</h1>
+        <TodoForm addTodo={addTodo} />
+        {todos.map((todo, index) => (
+            todo.isEditing ? (
+                <EditTodoForm editTodo={editTask} task={todo} />
+            ) : (
+                <Todo task={todo} key={index} toggleComplete={toggleComplete} deleteTodo={deleteTodo} editTodo={editTodo} />
+            )
+            
+        ))}
+         
+    </div>
+  )
+}
